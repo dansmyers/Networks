0ea9410d8dfa9650c7279ccc0b8216ff1a20ddde
nvuckovic <nvuckovic@rollins.edu>
2020-10-29 01:07:01 +0000

    deliverables upload

--- Sprint-3-IP_and_Back_End/Deliverables/app.js
+++ Sprint-3-IP_and_Back_End/Deliverables/app.js
@@ -20,6 +20,99 @@ app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/index.html')));
 // Load required packages
 const fs = require('fs');
 
+let index = {};
+let play = '';
+let act = '';
+let scene = '';
+let actor = '';
+let phrase = '';
+    
+// List of the plays
+let texts = ['macbeth.txt', 'romeo_and_juliet.txt', 'a_midsummer_nights_dream.txt'];
+    
+// Read the contents of each text, then call the readText function
+//
+// Note: readText runs asynchronously, so you can't guarantee that the 
+// callbacks execute in any particular order
+//
+// Also note: this version reads the entire text of the file into one
+// string, which may not always be what you want. There are other options
+// to read files line by line, but you have to use a PER-LINE callback.
+    
+for (let text of texts) {
+    fs.readFile('./texts/' + text, 'utf8' , readText);
+}
+
+/**
+ * Process the complete text of one file
+ */
+ 
+function addLocation(key,line) {
+    if(key.charAt(0) !== key.charAt(0).toLowerCase()){
+        key = lowerCase(key);
+    }
+    
+    if(key in index){
+        let info = play + '\n' + act + ', ' + scene + '\n' + actor + '\n' + line + '\n';
+        index[key].locations.push(info);
+    }
+    else{
+        index[key] = {locations: []};
+        let info = play + '\n' + act + ', ' + scene + '\n' + actor + '\n' + line + '\n';
+        index[key].locations.push(info);
+    }
+}
+
+function lowerCase(key) {
+    return key.charAt(0).toLowerCase() + key.slice(1);
+}
+
+function readText(err, fullText) {
+    
+    
+    if (err) {
+        console.log(err);
+        return;
+    }
+    let lines = fullText.split(/\r?\n/);
+    play = lines[0]; 
+    // Process each line
+    for (let line of lines) { 
+    
+        
+        words = line.split((/[\s,;:.!?]+/));
+        for(let word of words){
+            if(isNaN(word.charCodeAt(0))){
+            }
+            else if(line === lines[0]){
+                
+            } 
+            else if(line.substring(0,3) === 'ACT'){
+                act = line;
+            }
+            else if(line.substring(0,5) === 'SCENE'){
+                let splitter = words[1].split('\t');
+                scene = words[0] + ' ' + splitter[0];
+            }
+            else if(line.charAt(0) !== '\t'){ 
+                let splitter = words[0].split('\t');
+                actor = splitter[0];
+                
+                if(word === words[0]){
+                    addLocation(word,line);
+                }
+                else{
+                    addLocation(word,line);
+                }
+            }
+            else if(line.charAt(0) === '\t'){
+                addLocation(word,line);
+            }
+        }
+    }
+    
+}
+
 
 
 // Add submit route to receive requests from the example page
@@ -28,98 +121,17 @@ app.get('/submit', function(req, res) {
     
     let name = req.query.name;
     console.log("Name: " + name);
-    /**
-     * Main -- setup the index
-     */ 
-     
-    // Index object
-    let index = {};
-    
-    // List of the plays
-    let texts = ['macbeth.txt', 'romeo_and_juliet.txt', 'a_midsummer_nights_dream.txt'];
-    
-    // Read the contents of each text, then call the readText function
-    //
-    // Note: readText runs asynchronously, so you can't guarantee that the 
-    // callbacks execute in any particular order
-    //
-    // Also note: this version reads the entire text of the file into one
-    // string, which may not always be what you want. There are other options
-    // to read files line by line, but you have to use a PER-LINE callback.
-    
-    // for (let text of texts) {
-       fs.readFile('./texts/' + texts[2], 'utf8' , readText);
-    // }
-});
-
-/**
- * Process the complete text of one file
- */
-function readText(err, fullText) {
-    
-    // If there was an error, give up
-    if (err) {
-        console.log(err);
-        return;
-    }
-    
-    let searchterm = 'methinks';
-    
-    let play = '';
-    let act = '';
-    let scene = '';
-    let actor = '';
-    let phrase = '';
-    
-    // Split the complete text, which is one huge string,
-    // into separate lines using a regular expression
-    let lines = fullText.split(/\r?\n/)
-    
-    play = lines[0]; // get play
-    
-    // Process each line
-    for (let i = 0; i < 15; i++) { // let line of lines
-    
-        let words = lines[i].split('\t');
-        
-        if(lines[i].charAt(0) !== '\t'){
-            words = lines[i].split(/[\s\t]+/);
-            if(words[0] === 'ACT'){
-                act = words[0] + ' ' + words[1]; // update act
-            }
-            if(words[0] === 'SCENE'){
-                scene = words[0] + ' ' + words[1]; // update scene 
-            }
-            if(words.length > 1){
-                actor = words[0]; // update actor
-            }
-            //console.log(words[0] + ' ');
-            //console.log(i+1 + ' ' + lines[i]);
-        }
-        if(lines[i].charAt(0) === '\t'){
-            words = lines[i].split(/[\s\t,;:.]+/);
-            for(let word of words){
-                console.log(word);
-                if(word === searchterm){
-                    phrase = lines[i]; // get line
-                }
-            }
-        }
-   
-        //console.log(i + ' ' + lines[i]);
-        
-        //console.log(lines[i]);
-    }
-    console.log();
-    console.log(play);
-    console.log(act);
-    console.log(scene);
-    console.log(actor);
-    console.log(phrase);
     
     res.setHeader('Content-Type', 'application/json');
-    res.json(index);
-}
+    
+    let results = index[name];
+    let response = results;
+
+    
+    
+    res.json(response);
+
+});
 
 // Run server
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
