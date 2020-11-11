e167b278287e71b9544f1e5361a0494d16d743be
jaysarc <jramirez@rollins.edu>
2020-11-11 03:04:07 +0000

    added index/app

--- Sprint-3-IP_and_Back_End/Deliverables/app.js
+++ Sprint-3-IP_and_Back_End/Deliverables/app.js
@@ -1,52 +1,142 @@
-/**
- * Search engine starter code
- */
+/* shakespeare search engine */
+
+// include modules
+const express = require('express');
+const app = express();
+const port = 80;
+var path = require('path');
+app.use(express.static('public'));
+
+// parser for JSON
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// return root page
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/index.html')));
 
 
-// Load required packages
+// load required packages
 const fs = require('fs');
 
+/* main: set up the index */
 
-/**
- * Process the complete text of one file
- */
+let index = {};
+let play = '';
+let act = '';
+let scene = '';
+let character = '';
+let phrase = '';
+    
+// list of shakespeare plays
+let texts = ['macbeth.txt', 'romeo_and_juliet.txt', 'a_midsummer_nights_dream.txt'];
+    
+// read in the texts
+for (let text of texts) {
+    fs.readFile('./texts/' + text, 'utf8' , readText);
+}
+
+/* text processing */
+ 
+function addLocation(key,line) {
+    
+    // case sensitivity, check for capitalization
+    if(key.charAt(0) !== key.charAt(0).toLowerCase()){
+        key = key.toLowerCase(); // convert to lowercase
+    }
+    
+    if(key in index){
+        let info = play + '\n' + act + ', ' + scene + '\n' + character + '\n' + line + '\n';
+        index[key].locations.push(info);
+    }
+    else {
+        index[key] = {locations: []};
+        let info = play + '\n' + act + ', ' + scene + '\n' + character + '\n' + line + '\n';
+        index[key].locations.push(info);
+    }
+}
+
 function readText(err, fullText) {
     
-    // If there was an error, give up
+    // if there was an error, give up
     if (err) {
         console.log(err);
         return;
     }
     
-    // Split the complete text, which is one huge string,
-    // into separate lines using a regular expression
-    let lines = fullText.split(/\r?\n/)
+    // split the text into separate lines
+    let lines = fullText.split(/\r?\n/);
     
-    // Process each line
+    play = lines[0]; // get the play
+    
+    
+    // process each line
     for (let line of lines) {
-        console.log(line);
+        
+        // split based on spaces, punctuation
+        words = line.split((/[\s,.:;?!]+/));
+        
+        for(let word of words){
+            
+            // check for NaN
+            if(isNaN(word.charCodeAt(0))){
+                // trap
+            }
+            
+            // title
+            else if(line === lines[0]){
+                // trap
+            }
+            
+            // identify act number
+            else if(line.substring(0,3) === 'ACT'){
+                act = line;
+            }
+            
+            // identify scene number
+            else if(line.substring(0,5) === 'SCENE'){
+                let sub = words[1].split('\t');
+                scene = words[0] + ' ' + sub[0];
+            }
+            
+            // identify character
+            else if(line.charAt(0) !== '\t'){ //&& !(isNaN(word.charCodeAt(0)))
+                
+                let sub = words[0].split('\t');
+                character = sub[0];
+                
+                // if the character is combined with the first word
+                if(word === words[0]){
+                    addLocation(word, line);
+                }
+                
+                else{
+                    addLocation(word, line);
+                }
+            }
+            
+            else if(line.charAt(0) === '\t'){
+                addLocation(word, line);
+            }
+        }
     }
 }
 
+// submit route for receiving requests
+app.get('/submit', function(req, res) {
+    console.log('Request received');
+    
+    let name = req.query.name;
+    console.log("Word: " + name);
+    
+    res.setHeader('Content-Type', 'application/json');
+    
+    let results = index[name];
+    let response = results;
+    
+    res.json(response);
 
-/**
- * Main -- setup the index
- */ 
- 
-// Index object
-let index = {}
+});
 
-// List of the plays
-let texts = ['macbeth.txt', 'romeo_and_juliet.txt', 'a_midsummer_nights_dream.txt']
-
-// Read the contents of each text, then call the readText function
-//
-// Note: readText runs asynchronously, so you can't guarantee that the 
-// callbacks execute in any particular order
-//
-// Also note: this version reads the entire text of the file into one
-// string, which may not always be what you want. There are other options
-// to read files line by line, but you have to use a PER-LINE callback.
-for (let text of texts) {
-   fs.readFile('./texts/' + text, 'utf8' , readText);
-}
+// run server
+app.listen(port, () => console.log(`Shakespeare Search Engine listening on port ${port}!`));
