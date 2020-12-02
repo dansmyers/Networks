e3e58f49cfa172348371d2794ed0a7d3c551f9c7
root <samjsolomon@aol.com>
2020-12-02 16:55:18 +0000

    sprint 5

---
+++ Sprint-5-Security/project-checkin/joke.js
@@ -0,0 +1,17 @@
+// dad joke ---------------------------------------------------------------------//
+function getJoke() {
+fetch(`https://icanhazdadjoke.com/graphql?query=query{joke{joke}}`)
+	.then(response =>  response.json())
+	.then(res => {
+		const {data} = res;
+		let joke = data.joke.joke;
+		document.querySelector('.joke').innerHTML = joke;
+	})
+
+}
+
+getJoke();
+
+document.querySelector('.btn').addEventListener('click', () => {
+	getJoke();
+});
---
+++ Sprint-5-Security/project-checkin/weather.js
@@ -0,0 +1,57 @@
+
+window.addEventListener('load', ()=>{
+//set up important variables
+let lon;
+let lat;
+let tempDesc = document.querySelector('.temp-description');
+let tempDegree = document.querySelector('.temp-degree');
+let locationZone = document.querySelector('.location-timezone');
+
+if(navigator.geolocation){
+    navigator.geolocation.getCurrentPosition(position =>{
+        lon = position.coords.longitude;
+        lat = position.coords.latitude;
+        const apiKey = '4d9534f3a14a0f2346837165b7dd3223';
+        const api = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}` ;
+        const api2 = `https://nominatim.openstreetmap.org/reverse?format=json&lat=${lat}&lon=${lon}&zoom=18&addressdetails=1`
+        fetch(api)
+        .then(response =>{
+            return response.json();
+        })
+        .then(data =>{
+          //grab temp, summary, and icon from JSON
+          const temp = data.main.temp;
+          const summary = data.weather[0].description;
+          const icon = data.weather[0].icon;
+          const iconURL = `http://openweathermap.org/img/w/${icon}.png`
+
+
+          //set temp, summary, and icon
+          tempDegree.textContent = Math.floor(temp);
+          tempDesc.textContent = summary;
+          document.querySelector('.icon').src = iconURL;
+        })
+
+          // update location
+        fetch(api2)
+        .then(response =>{
+            return response.json();
+        })
+        .then(data =>{
+          //grab location
+            const {city, neighbourhood, state} = data.address
+            abbrState(state,'abbr');
+
+            locationZone.textContent = neighbourhood + ", " + city;
+
+        })
+
+    });
+
+
+}else{
+    h1.textContent = "Can't get location"
+}
+
+
+});
