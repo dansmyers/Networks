971a8a7f3236d2aeadf3f87e99376249c583be2c
RLSpringer <46799025+RLSpringer@users.noreply.github.com>
2020-12-11 09:25:35 -0500

    Update main.js

--- Challenge-Projects/Electron/main.js
+++ Challenge-Projects/Electron/main.js
@@ -2,7 +2,7 @@ const electron = require('electron');
 const url = require('url');
 const path = require('path');
 
-const {app, BrowserWindow} = electron;
+const {app, BrowserWindow, Menu, ipcMain} = electron;
 
 let mainWindow;
 let addWindow;
