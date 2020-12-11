cce84813e183e890b446bb279244cfb880b13b99
nitishjagdev <46824620+nitishjagdev@users.noreply.github.com>
2020-12-11 00:33:16 -0500

    Add files via upload

---
+++ Challenge-Projects/TicTac (1).js
@@ -0,0 +1,77 @@
+function Square(props){
+  return(
+  <button className="square" onClick={props.onClick}>
+      {props.value}
+      </button>
+    );
+}
+
+class Board extends React.Component {
+  constructor(props){
+    super(props);
+    this.state={
+      squares: Array(9).fill(null),
+    };
+  }
+  handleClick(i){
+    const squares=this.state.squares.slice();
+    squares[i]='X';
+    this.setState({squares:squares});
+  }
+  renderSquare(i) {
+
+    return( <Square 
+              value= {this.state.squares[i]} 
+             onClick={()=>this.handleClick(i)}
+             />
+      );
+  }
+
+  render() {
+    const status = 'Next player: X';
+
+    return (
+      <div>
+        <div className="status">{status}</div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </div>
+    );
+  }
+}
+
+class Game extends React.Component {
+  render() {
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board />
+        </div>
+        <div className="game-info">
+          <div>{/* status */}</div>
+          <ol>{/* TODO */}</ol>
+        </div>
+      </div>
+    );
+  }
+}
+
+// ========================================
+
+ReactDOM.render(
+  <Game />,
+  document.getElementById('root')
+);
