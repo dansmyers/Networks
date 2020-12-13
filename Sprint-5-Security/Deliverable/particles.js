7f5d70becaef80fc29f1cb481920667c3ad2a2f5
Oliver Luo <oluo@rollins.edu>
2020-12-13 23:56:22 +0000

    added search bar with autocomplete, need to switch to jquery tho

--- Sprint-5-Security/Deliverable/particles.js
+++ Sprint-5-Security/Deliverable/particles.js
@@ -43,30 +43,3 @@ cloudyButton.addEventListener('click', function() {
     tsParticles.dom()[0].refresh();
 })
 
-/* Dropdown Menu */
-let elements = document.getElementsByClassName('dropdown-item');
-
-Array.from(elements).forEach((element) => {
-    element.addEventListener('click', (event) => {
-
-        currentCityID = cities.get(event.target.textContent);
-        console.log(currentCityID);
-        weatherBalloon( currentCityID );
-
-    });
-});
-
-/* Future City Hashmap Index */
-let cities = new Map();
-cities.set('Orlando', 4167147);
-cities.set('New York City', 5128581);
-cities.set('Piedmont', 4083004);
-cities.set('Osaka', 1853908);
-cities.set('Atlanta', 4180439);
-cities.set('Selmer', 4656442);
-cities.set('Tokyo', 1850147);
-cities.set('London', 2643743);5780993
-cities.set('Salt Lake City', 5780993);
-cities.set('Egypt', 357994);
-
-let currentCityID = 4167147;
